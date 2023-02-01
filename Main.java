a793f47377a784b0c937d6b77e000c35914cc340
Amir Hamza <123008407+amir160s@users.noreply.github.com>
2023-02-01 22:55:29 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    int x = 5;
+    float a=12.3554f;
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        Main obj= new Main();
+        System.out.println(myObj.x+"\n"+obj.a);
+
+    }
+}
